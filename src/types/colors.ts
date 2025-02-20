136187beaf0f914159fdf3285eabce3be71abe16
Abdulla <abdulla200532@gmail.com>
2025-02-20 17:00:01 +0000

    feat: colors object

--- src/types/colors.ts
+++ src/types/colors.ts
@@ -1,10 +1,26 @@
 export type ColorTypes =
-  | "--color-primary"
-  | "--color-secondary"
-  | "--color-black"
-  | "--color-white"
-  | "--color-gray-10"
-  | "--color-gray-25"
-  | "--color-gray-50"
-  | "--color-gray-75"
-  | "--color-gray-100";
+  | "primary"
+  | "secondary"
+  | "black"
+  | "white"
+  | "gray-10"
+  | "gray-25"
+  | "gray-50"
+  | "gray-75"
+  | "gray-100";
+
+type TColors = {
+  [key in ColorTypes]: string;
+};
+
+export const Colors: TColors = {
+  primary: "#3e0930",
+  secondary: "#db005f",
+  black: "#14000f",
+  white: "#f0f0f0",
+  "gray-10": "#edeced",
+  "gray-25": "#d0cfd0",
+  "gray-50": "#a39fa2",
+  "gray-75": "#746f73",
+  "gray-100": "#463e44",
+};
