6d60b48bd11fc34553cd645b5d3e4cbdf7352052
Abdulla <abdulla200532@gmail.com>
2025-02-20 16:58:24 +0000

    feat: colors and types

---
+++ src/types/colors.ts
@@ -0,0 +1,10 @@
+export type ColorTypes =
+  | "--color-primary"
+  | "--color-secondary"
+  | "--color-black"
+  | "--color-white"
+  | "--color-gray-10"
+  | "--color-gray-25"
+  | "--color-gray-50"
+  | "--color-gray-75"
+  | "--color-gray-100";
