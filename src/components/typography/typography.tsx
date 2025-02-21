f731735ce1bca1a2cd70aad99c88061b340678f1
Abdulla <abdulla200532@gmail.com>
2025-02-21 11:40:51 +0500

    fix: reexport styles

--- src/components/typography/typography.tsx
+++ src/components/typography/typography.tsx
@@ -1,47 +1,48 @@
-import type {Align, ColorTypes, Variant, Weight} from './types'
+import type { Align, ColorTypes, Variant, Weight } from "./types";
 
-import {ComponentPropsWithoutRef, forwardRef} from 'react'
+import { ComponentPropsWithoutRef, forwardRef } from "react";
 
-import {Slot} from '@radix-ui/react-slot'
-import {clsx} from 'clsx'
+import { Slot } from "@radix-ui/react-slot";
+import { clsx } from "clsx";
 
-import * as s from './typography.module.scss'
+import * as s from "./typography.module.scss";
 
 type Props = {
-    align?: Align
-    asChild?: boolean
-    color?: ColorTypes
-    variant?: Variant
-    weight?: Weight
-} & ComponentPropsWithoutRef<'span'>
+  align?: Align;
+  asChild?: boolean;
+  color?: ColorTypes;
+  variant?: Variant;
+  weight?: Weight;
+} & ComponentPropsWithoutRef<"span">;
 
 export const Typography = forwardRef<HTMLSpanElement, Props>(
-    (
-        {
-            align = 'inherit',
-            asChild = false,
-            className,
-            color = 'gray-10',
-            variant = 'regular14',
-            weight = 'regular',
-            ...props
-        },
-        ref
-    ) => {
-        const Comp = asChild ? Slot : 'span'
+  (
+    {
+      align = "inherit",
+      asChild = false,
+      className,
+      color = "gray-10",
+      variant = "regular14",
+      weight = "regular",
+      ...props
+    },
+    ref
+  ) => {
+    const Comp = asChild ? Slot : "span";
+    console.log(`color-${color}`);
 
-        return (
-            <Comp
-                className={clsx(
-                    s[variant],
-                    color && s[`color-${color}`],
-                    align && s[`text-align-${align}`],
-                    weight && s[`font-weight-${weight}`],
-                    className
-                )}
-                ref={ref}
-                {...props}
-            />
-        )
-    }
-)
+    return (
+      <Comp
+        className={clsx(
+          s[variant],
+          s[`color-${color}`],
+          align && s[`text-align-${align}`],
+          weight && s[`font-weight-${weight}`],
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
