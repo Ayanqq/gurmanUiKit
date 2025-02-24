5722cacd66268d9c3e555a3240dcb48d27bc7f0f
Abdulla <abdulla200532@gmail.com>
2025-02-24 11:38:57 +0500

    feat: footer tablet done

--- src/components/footer/footer_pc.stories.tsx
+++ src/components/footer/footer_pc.stories.tsx
@@ -1,12 +1,12 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { Footer_PC } from "./index";
+import { Footer_Tablet } from "./index";
 
 const meta = {
-  component: Footer_PC,
+  component: Footer_Tablet,
   tags: ["autodocs"],
   title: "Components/Footer",
-} satisfies Meta<typeof Footer_PC>;
+} satisfies Meta<typeof Footer_Tablet>;
 
 export default meta;
 
--- src/components/footer/footer_tablet.tsx
+++ src/components/footer/footer_tablet.tsx
@@ -14,83 +14,91 @@ export const Footer_Tablet = () => {
         <nav className={s.footerNav}>
           <div className={s.navItemLogoSection}>
             <img src={logo_img} alt="Gurman" className={s.logo} />
-            <Typography size="24" weight="bold">
+            <Typography size="20" weight="bold">
               Развивайте бизнес вместе с нами!
+            </Typography>
+          </div>
+          <div className={s.navItem}>
+            <Typography size="20" weight="bold">
+              Навигация
+            </Typography>
+            <div className={s.links}>
+              <a href="#">
+                <Typography size="16">Главная</Typography>
+              </a>
+              <a href="#">
+                <Typography size="16">Партнеры</Typography>
+              </a>
+              <a href="#">
+                <Typography size="16">Связаться</Typography>
+              </a>
+              <a href="#">
+                <Typography size="16">Преимущества</Typography>
+              </a>
+              <a href="#">
+                <Typography size="16">Контакты</Typography>
+              </a>
+            </div>
+          </div>
+        </nav>
+
+        <hr />
+
+        <div className={s.footerBottom}>
+          <div className={s.navItem}>
+            <Typography size="20" weight="bold">
+              Социальные сети
             </Typography>
             <div className={s.socials}>
               <a href="#">
-                <img className={s.socialIcon} src={inst_img} alt="Instagram" />
+                <img
+                  className={s.socialIcon}
+                  src={inst_img}
+                  alt="Instagram"
+                  height={40}
+                />
               </a>
               <a href="#">
                 <img
                   className={s.socialIcon}
                   src={whatsapp_img}
                   alt="Whatsapp"
+                  height={40}
                 />
               </a>
               <a href="#">
-                <img className={s.socialIcon} src={tg_img} alt="Telegram" />
+                <img
+                  className={s.socialIcon}
+                  src={tg_img}
+                  alt="Telegram"
+                  height={40}
+                />
               </a>
             </div>
           </div>
           <div className={s.navItem}>
-            <Typography size="24" weight="bold">
-              Навигация
+            <Typography size="20" weight="bold">
+              Скачайте наше приложение:
             </Typography>
-            <div className={s.links}>
+            <div className={s.downloadLinks}>
               <a href="#">
-                <Typography size="20">Главная</Typography>
+                <img
+                  src={app_store_img}
+                  alt="Download on App Store"
+                  height={60}
+                />
               </a>
               <a href="#">
-                <Typography size="20">Партнеры</Typography>
-              </a>
-              <a href="#">
-                <Typography size="20">Связаться</Typography>
-              </a>
-              <a href="#">
-                <Typography size="20">Преимущества</Typography>
-              </a>
-              <a href="#">
-                <Typography size="20">Контакты</Typography>
+                <img
+                  src={google_play_img}
+                  alt="Download on Google"
+                  height={60}
+                />
               </a>
             </div>
           </div>
-          <div className={s.navItem}>
-            <Typography size="24" weight="bold">
-              Партнерам
-            </Typography>
-            <div className={s.links}>
-              <a href="#">
-                <Typography size="20">Для вашего бизнеса</Typography>
-              </a>
-            </div>
-          </div>
-          <div className={s.navItem}>
-            <Typography size="24" weight="bold">
-              Клиентам
-            </Typography>
-            <div className={s.links}>
-              <a href="#">
-                <Typography size="20">Обратная связь</Typography>
-              </a>
-            </div>
-          </div>
-        </nav>
-        <hr />
-      </footer>
-      <div className={s.footerBottom}>
-        <Typography size="24" weight="bold">
-          Скачайте наше приложение:
-        </Typography>
-        <div className={s.downloadLinks}>
-          <a href="#">
-            <img src={app_store_img} alt="Download on App Store" height={68} />
-          </a>
-          <a href="#">
-            <img src={google_play_img} alt="Download on Google" height={68} />
-          </a>
         </div>
-      </div>
+      </footer>
     </div>
   );
 };
